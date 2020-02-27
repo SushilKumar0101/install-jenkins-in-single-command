f5326d619a090d15402623d85c7c8f00ce4d79d5
SushilKumar0101 <57595697+SushilKumar0101@users.noreply.github.com>
2020-02-27 14:34:51 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt install openjdk-11-jdk
+sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+echo "your user key:"
+sudo /var/lib/jenkins/secrets/initialAdminPassword
